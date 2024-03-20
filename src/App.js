5a0bb217ada0587e5a87ae867954c98b3143e5e5
karan <karanpatel2329@gmail.com>
2024-03-21 00:43:45 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function App() {
+  useEffect(() => {
+    document.title = 'My Page Title';
+  }, []);
   return (
     <div style={{
       padding: '30px',
